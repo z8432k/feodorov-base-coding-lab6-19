e8e690b54d19add1cc55664569ed3be5ea621081
Alexander Feodorov <webmaster@unixcomp.org>
2019-12-28 18:27:22 +0300

    Solved

--- src/matrix.cc
+++ src/matrix.cc
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include <glib.h>
+#include <math.h>
 #include <iostream>
 
 using namespace std;
@@ -20,37 +21,42 @@ typedef struct {
 typedef Matrix_t* Matrix;
 
 Matrix allocMatrix(const gsize dim);
-int inputMatrix(Matrix matrix);
+int inputMatrix(Matrix *matrix);
 void freeMatrix(Matrix matrix);
 template <typename T>
 static void inputValue(T *value, const char* errStr);
 
+void printPositiveRows(Matrix matrix);
+void minDiagSum(Matrix matrix);
+
 int main(void) {
-  printf("= Two dimension matrix processing. =\n");
+  cout << "= Two dimension matrix processing. =" << endl;
 
   Matrix matrix;
 
-  inputMatrix(matrix);
+  inputMatrix(&matrix);
+  printPositiveRows(matrix);
+  minDiagSum(matrix);
 
   exit(EXIT_SUCCESS);
 }
 
-int inputMatrix(Matrix matrix) {
+int inputMatrix(Matrix *matrix) {
   gsize dim;
 
   cout << "Input matrix dimension: ";
   inputValue(&dim, "Wrong dimension value. Try again.");
 
-  matrix = allocMatrix(dim);
+  *matrix = allocMatrix(dim);
 
   cout << "Input all matrix values in С style (by rows) one by one and press enter after each." << endl;
 
   gsize i = 0, j = 0, k = 0;
   do {
     cout << "Input cell ["<< i <<"][" << j << "]"<< endl;
-    inputValue(&matrix->data[k], "Wrong matrix value. Try again.\n");
+    inputValue(&(*matrix)->data[k], "Wrong matrix value. Try again.\n");
 
-    if (j == matrix->dim - 1) {
+    if (j == dim - 1) {
       i++;
       j = 0;
     }
@@ -59,7 +65,7 @@ int inputMatrix(Matrix matrix) {
     }
 
     k++;
-  } while (k < matrix->dim * matrix->dim);
+  } while (k < dim * dim);
 
   return 0;
 }
@@ -89,4 +95,72 @@ static void inputValue(T *value, const char* errStr) {
     cerr << errStr;
     cin >> *value;
   }
+}
+
+void printPositiveRows(Matrix matrix) {
+  cout << endl;
+
+  for (gsize i = 0; i < matrix->dim; i++) {
+    int sum = 0;
+
+    for (gsize j = 0; j < matrix->dim; j++) {
+      gsize idx = matrix->dim * i + j;
+      int val = matrix->data[idx];
+
+      if (val < 0) {
+        cout << "Row: " << i << " has negative number." << endl;
+        goto nextrow;
+      }
+      else {
+        sum += val;
+      }
+    }
+
+    cout << "Row: " << i << ", sum: " << sum << "." << endl;
+
+    nextrow:
+    ;
+  }
+}
+
+
+void minDiagSum(Matrix matrix) {
+  gsize vectorLUsize = matrix->dim * 2 - 2;
+  int *vectorLU = g_new0(int, vectorLUsize);
+  gsize Uoffset = matrix->dim - 1;
+
+  for (gsize i = 0; i < matrix->dim; i++) {
+    for (gsize j = 0; j < matrix->dim; j++) {
+      gsize idx = matrix->dim * i + j;
+      int val = matrix->data[idx];
+
+      if (i < j) {
+        gsize Upos = Uoffset + j - 1 - i;
+        vectorLU[Upos] += val;
+      }
+      else if (i > j) {
+        gsize Lpos = i - 1 - j;
+        vectorLU[Lpos] += val;
+      }
+    }
+  }
+
+  cout << endl;
+
+  int minimum = INT_MAX;
+
+  for (gsize i = 0; i < vectorLUsize; i++) {
+    if (i < vectorLUsize / 2) {
+      cout << "L" << i + 1 << "sum = ";
+    }
+    else {
+      cout << "U" << i - vectorLUsize / 2 + 1 << "sum = ";
+    }
+
+    cout << vectorLU[i] << endl;
+
+    minimum = min(minimum, vectorLU[i]);
+  }
+
+  cout << endl << "Minimum diag sum: " << minimum << endl;
 }
